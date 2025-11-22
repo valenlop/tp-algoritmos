917063a48d4d46bd7c4117afec4871f8d4b09d27
Valen <vlopezr@fi.uba.ar>
2025-11-22 01:43:12 -0300

    cambios 22-11 1:42am

--- idea2.c
+++ idea2.c
@@ -1,1 +1,30 @@
 // Desglose de idea ((mas detalle))
+
+/*
+Cosas que tengo:
+
+color_h: Formato SHL y RGB util para definir los tetraminos de colores y dibujar con SDL2 (ya hecho)
+
+imagen_h: Manejar el tablero como imagen, pegar piezas, leer/escribir PPM para testear. (falta completar)
+    Tengo que adaptar matriz.h (funciones)
+    falta:  - imagen_setear_pixel
+            - imagen_establecer_pixel
+            - imagen_pegar_no_negros
+            - imagen_rotar
+
+sprite_h/sprites_h: Leer tetraminos y textos (numeros, letras) + proximas piezas y puntaje (ya hecho)
+
+
+TDAs a implementar (tal vez):
+    
+    - pieza.h: Una pieza actual (la pieza que cae). La pieza y el tablero son dos cosas distintas.
+
+
+    - tablero.h: Laburo del tablero. Va la imagen de fondo y los tetraminos que chocaron.
+
+
+    - juego.h: Controla toda la logica de juego. Movimientos de las piezas, tiempo y demas dibujos en pantalla.
+
+
+
+*/
---
+++ tdas.c
@@ -0,0 +1,45 @@
+// Voy a detallar los tdas con las funciones que creo que deben tener
+
+/*
+
+- pieza.h: Una pieza actual (la pieza que cae).
+
+- juego.h: 
+
+- tablero.h: 
+
+*/
+
+
+
+
+// TDAs que ya tengo
+
+/*
+
+- color.h:
+
+- imagen.h:
+
+- sprite.h
+
+- sprites.h:
+
+*/
+
+
+// Cosas de gran utilidad ((realmente necesarias))
+
+/*
+
+- matriz.h:
+
+- config.h:
+
+- test_sdl2.c: 
+
+- fondo.ppm:
+
+sprites.bin: 
+
+*/
